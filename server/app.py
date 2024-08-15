a5473d836d2b80726f01e580140f2fccb71360a3
Gray Hanna <ghostblossom54288@gmail.com>
2024-08-15 10:30:34 -0600

    tables done and almost all crub

--- server/app.py
+++ server/app.py
@@ -1,8 +1,47 @@
-from flask import request
+from flask import Flask, request, make_response, jsonify
+from flask_migrate import Migrate
 from flask_restful import Resource, Api
-
+from models import Activity, Luggage, Vacation, User, Trip
 from config import app, db, api
 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
+
+class All_Luggage(Resource):
+    def get(self, id):
+        ll = Luggage.query.all()
+        return [lug.to_dict() for lug in ll],200
+    def post(self):
+        try:
+            data = request.get_json()
+            lugg = Luggage(
+                pants = data['pants'], 
+                shirts = data['shirts'], 
+                other_clothes = data['other_clothes']
+            )
+            db.session.add(lugg)
+            db.session.commit()
+            return lugg.to_dict(), 200
+        except Exception as e:
+            raise ValueError('New bag as your carry on')
+api.add_resource(All_Luggage,'/Luggages')
+
+class One_Lugg(Resource):
+    def get(self, id):
+        lug = Luggage.query.filter(Luggage.id == id).first()
+        if lug:
+            return lug.to_dict(), 200
+
+api.add_resource(All_Signups,'/signups')
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -7,15 +7,34 @@ class Activity(db.Model, SerializerMixin):
     __tablename__ = 'activities'
     
     id = db.Column(db.Integer, primary_key=True)
-    budget = db.Column(db.Integer)
-    adv_scale = db.Column(db.Integer)
-    #a_o = alone or with others
-    a_o = db.Column(db.Boolean)
+    budget = db.Column(db.Integer, nullable=False)
+    adv_scale = db.Column(db.Integer , nullable=False)
+    is_alone = db.Column(db.Boolean)
     name = db.Column(db.String)
     desc = db.Column(db.String)
     
     vacations = relationship('Vacation', back_populates='activity', cascade ="all, delete-orphan")
 
+    @validates('budget')
+    def check_bank(self, key, value):
+        if 0<= value => 350 and type(value) is str:
+            return value
+        else:
+            raise ValueError('Get more money brokey')
+
+    @validates('adv_scale')
+    def check_adv(self, key, value):
+        if type(value) is str and 1<= value =< 10:
+            return value
+        else:
+            raise ValueError('Input a number between 1-10')
+
+    @validates('name', 'desc')
+    def check_two(self, key, value):
+        if type(value) is str:
+            return value
+        else:
+            raise ValueError('Oops, something was spelled wrong or the wrong type was inputed')
 
 class Vacation(db.Model, SerializerMixin):
     __tablename__ = 'vacations'
@@ -32,15 +51,33 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    budget = db.Column(db.Float)
-    a_o = db.Column(db.Boolean)
-    password = db.Column(db.String)
-    email = db.Column(db.String)
+    username = db.Column(db.String , nullable=False)
+    budget = db.Column(db.Float, nullable=False)
+    is_alone = db.Column(db.Boolean)
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique = True)
 
     luggages = relationship('Luggage', back_populates = 'user', cascade ="all, delete-orphan")
     vacations = relationship('Vacation', back_populates = 'user', cascade ="all, delete-orphan")
 
+    @validates('password')
+    def check_password(self,key, value):
+        if type(value) is str and 1 >= value =< 8:
+            return value
+        else:
+            raise ValueError('Make your passowrd 8 letters')
+    @validates('username')
+    def check_username(self, key, value):
+        if type(value) is str:
+            return value
+        else:
+            raise ValueError('Not a vaild username')
+    @validates('budget')
+    def check_budget(self,key, value):
+        if type(value) is float:
+            return value 
+        else:
+            raise ValueError('You are too broke :(')
 
 class Luggage(db.Model, SerializerMixin):
     __tablename__ = 'luggages'
@@ -50,18 +87,69 @@ class Luggage(db.Model, SerializerMixin):
     style_pants = db.Column(db.Boolean)
     style_accessories = db.Column(db.Boolean)
     is_summer = db.Column(db.Boolean)
+    pants = db.Column(db.Integer)
+    shirts = db.Column(db.Integer)
+    other_clothes = db.Column(db.Integer)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = relationship('User', back_populates = 'luggages')
+
+    @validates
+    def check_others(self,key,value):
+        if type(value) is int:
+            return value
+        else:
+            raise ValueError('Not enough clothes')
+    @validates
+    def check_shirts(self,key,value):
+        if type(value) is int:
+            return value
+        else:
+            raise ValueError('You need more shirts')
+    @validates
+    def check_pants(self,key,value):
+        if type(value) is int:
+            return value
+        else:
+            raise ValueError('You didnt pack the right amount')
 
 class Trip(db.Model, SerializerMixin):
     __tablename__ = 'trips'
 
     id = db.Column(db.Integer, primary_key=True)
-    location  = db.Column(db.String)
-    season = db.Column(db.String)
+    location  = db.Column(db.String, nullable=False)
+    season = db.Column(db.String, nullable=False)
     is_winter = db.Column(db.Boolean)
-    price = db.Column(db.Float)
+    price = db.Column(db.Float, nullable=False)
     is_flying = db.Column(db.Boolean)
-    weight_limit = db.Column(db.Integer)
-
+    weight_limit = db.Column(db.Integer, nullable=False)
+    
+    @validates('weight_limit')
+    def check_limit(self, key, value):
+        if type(value) is int and 25<=value=<50:
+            return value
+        else:
+            raise ValueError('You packed too much!')
+    @validates('price')
+    def check_price(self, key, value):
+        if type(value) is float:
+            return value
+        else:
+            raise ValueError('You need to spend more money')
+    @validates('location')
+    def check_location(self, key, value):
+        if type(value) is str:
+            return value
+        else:
+            raise ValueError('Not a vaild username')
+    @validates('season')
+    def check_season(self, key, value):
+        valid_season=["Summer","Spring", "Winter", "Fall"]
+        if type(value) is str:
+            return value
+            for letter in value:
+                if letter not in valid_season:
+                    raise ValueError("Not a season on earth")
+            return value
+        else:
+            raise ValueError('Not a vaild username')
